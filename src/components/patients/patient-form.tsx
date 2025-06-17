4347b254df6c6005613fff93a879aae27820ee50
Sachin Srivastava <sachinsrivastava.id@gmail.com>
2025-06-17 15:15:49 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/patients/patient-form.tsx
+++ src/components/patients/patient-form.tsx
@@ -450,33 +450,36 @@ export function PatientForm({ patient, onSubmit, isSubmitting }: PatientFormProp
               <FormField
                 control={form.control}
                 name="clinicalProfile.compliance"
-                render={({ field }) => (
-                  <FormItem className="space-y-3">
-                    <FormLabel><GripVertical className="inline h-4 w-4 mr-1"/>Compliance</FormLabel>
-                    <FormControl>
-                      <RadioGroup
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                        className="flex flex-row space-x-4"
-                        ref={field.ref} // ref added here
-                        name={field.name} // name added here
-                      >
-                        {YES_NO_UNKNOWN_OPTIONS.map(option => (
-                          <FormItem key={option} className="flex items-center space-x-2 space-y-0">
-                            <FormControl>
-                              <RadioGroupItem value={option} id={`compliance-${option.toLowerCase()}`} />
-                            </FormControl>
-                            <FormLabel htmlFor={`compliance-${option.toLowerCase()}`} className="font-normal cursor-pointer">
-                              {option}
-                            </FormLabel>
-                          </FormItem>
-                        ))}
-                      </RadioGroup>
-                    </FormControl>
-                    {/* <FormDescription> Select patient's compliance status. </FormDescription> */}
-                    <FormMessage />
-                  </FormItem>
-                )}
+                render={({ field }) => {
+                  const { formItemId, formDescriptionId, formMessageId, error } = useFormField();
+                  return (
+                    <FormItem className="space-y-3">
+                      <FormLabel htmlFor={formItemId}><GripVertical className="inline h-4 w-4 mr-1"/>Compliance</FormLabel>
+                      <FormControl>
+                        <RadioGroup
+                          ref={field.ref}
+                          name={field.name}
+                          onValueChange={field.onChange}
+                          value={field.value}
+                          className="flex flex-row space-x-4"
+                          id={formItemId}
+                          aria-describedby={!error ? formDescriptionId : `${formDescriptionId} ${formMessageId}`}
+                          aria-invalid={!!error}
+                        >
+                          {YES_NO_UNKNOWN_OPTIONS.map(option => (
+                            <FormItem key={option} className="flex items-center space-x-2 space-y-0">
+                              <RadioGroupItem value={option} id={`compliance-${option.toLowerCase()}-item`} />
+                              <FormLabel htmlFor={`compliance-${option.toLowerCase()}-item`} className="font-normal cursor-pointer">
+                                {option}
+                              </FormLabel>
+                            </FormItem>
+                          ))}
+                        </RadioGroup>
+                      </FormControl>
+                      <FormMessage id={formMessageId}/>
+                    </FormItem>
+                  );
+                }}
               />
             </div>
 
